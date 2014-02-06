95d767c25386d5873d1709ce8e91ece809385b2d
jharris14 <jharris14@punahou.edu>
2014-02-05 14:10:14 -1000

    Captain's Log: Stardate 02052014
    Rewrote the driver to implement each piece with its specified color and
    location in a bounded grid representing the chess board. Planned and
    researched setup of piece classes as well as brainstorming player-world
    interactions.

--- Driver/ChessDriver.java
+++ Driver/ChessDriver.java
@@ -1,14 +1,47 @@
-//Jaz Harris
+//Nolan Chung && Jaz Harris
 //Chess Driver
 
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public static class ChessDriver
 {
-  Scanner input = new Scanner (System.in);//creating a new scanner
-  System.out.println("Welcome, which game would you like to play?");//greeting the players
-  System.out.println("Enter 1 for normal chess, 2 for all knight-er, and 3 for Chad");
-  int n = input.nextInt();//creating an int for the players' response
-  if(n == 1)//Normal Chess game
-  {
-    
+  ActorWorld gettingBoard = new ActorWorld(new BoundedGrid(8,8));//making the chess board
+  gettingBoard.add(new Location(6, 0), new Pawn(white));//making a pawn piece setting it at the right location & color
+  gettingBoard.add(new Location(6, 1), new Pawn(white));
+  gettingBoard.add(new Location(6, 2), new Pawn(white));
+  gettingBoard.add(new Location(6, 3), new Pawn(white));
+  gettingBoard.add(new Location(6, 4), new Pawn(white));
+  gettingBoard.add(new Location(6, 5), new Pawn(white));
+  gettingBoard.add(new Location(6, 6), new Pawn(white));
+  gettingBoard.add(new Location(6, 7), new Pawn(white));
+  gettingBoard.add(new Location(7, 0), new Rook(white));//making a rook piece
+  gettingBoard.add(new Location(7, 7), new Rook(white));
+  gettingBoard.add(new Location(7, 1), new Knight(white));//making a knight piece
+  gettingBoard.add(new Location(7, 6), new Knight(white));
+  gettingBoard.add(new Location(7, 2), new Bishop(white));//making a bishop piece
+  gettingBoard.add(new Location(7, 5), new Bishop(white));
+  gettingBoard.add(new Location(7, 4), new King(white));//making a king piece
+  gettingBoard.add(new Location(7, 3), new Queen(white));//making a queen piece
+  gettingBoard.add(new Location(1, 0), new Pawn(black));//making the black pieces
+  gettingBoard.add(new Location(1, 1), new Pawn(black));
+  gettingBoard.add(new Location(1, 2), new Pawn(black));
+  gettingBoard.add(new Location(1, 3), new Pawn(black));
+  gettingBoard.add(new Location(1, 4), new Pawn(black));
+  gettingBoard.add(new Location(1, 5), new Pawn(black));
+  gettingBoard.add(new Location(1, 6), new Pawn(black));
+  gettingBoard.add(new Location(1, 7), new Pawn(black));
+  gettingBoard.add(new Location(0, 0), new Rook(black));//making a rook piece 
+  gettingBoard.add(new Location(0, 7), new Rook(black));
+  gettingBoard.add(new Location(0, 1), new Knight(black));//making a knight piece
+  gettingBoard.add(new Location(0, 6), new Knight(black));
+  gettingBoard.add(new Location(0, 2), new Bishop(black));//making a bishop piece
+  gettingBoard.add(new Location(0, 5), new Bishop(black));
+  gettingBoard.add(new Location(0, 4), new King(black));//making a king piece
+  gettingBoard.add(new Location(0, 3), new Queen(black));//making a queen piece
+  gettingBoard.show();
+}
