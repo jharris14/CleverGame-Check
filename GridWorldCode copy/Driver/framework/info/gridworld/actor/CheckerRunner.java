34d7266045d8f5a3c8a3ad8e8eb1420a2a8eb93f
jharris14 <jharris14@punahou.edu>
2014-02-20 23:28:04 -1000

    Stardate: 02202014
    After modifying the TestWorld to death, I am no closer to getting the
    stupid clicking method to work. Back to the drawing board, but I want
    to save this code lest I want to return to it later

--- GridWorldCode copy/Driver/framework/info/gridworld/actor/CheckerRunner.java
+++ GridWorldCode copy/Driver/framework/info/gridworld/actor/CheckerRunner.java
@@ -7,7 +7,6 @@ import info.gridworld.actor.Rock;
 import info.gridworld.actor.BlackSquare;
 import info.gridworld.actor.WhitePiece;
 import info.gridworld.actor.BlackPiece;
-import info.gridworld.actor.Critter;
 import info.gridworld.grid.Location;
 import info.gridworld.grid.BoundedGrid;
 
@@ -16,49 +15,49 @@ public class CheckerRunner
 {
   public static void main(String[] args)
     {
-    TestWorld gettingBoard = new TestWorld();
-    for(int i=0; i <= 2; i = i +2)
+    TestWorld gettingBoard = new TestWorld();//making a new TestWorld
+    for(int i=0; i <= 2; i = i +2)//for loop to add two rows of black pieces
     {
       for(int j=0; j <= 6; j = j +2)
       {
-        gettingBoard.add(new Location(i, j), new WhitePiece());
-      }
-    }
-    for(int i=1; i <= 2; i = i +2)
-    {
-      for(int j=1; j <= 7; j = j +2)
-      {
-        gettingBoard.add(new Location(i, j), new WhitePiece());
-      }
-    }
-    for(int i=4; i <= 7; i = i +2)
-    {
-      for(int j=0; j <= 7; j = j +2)
-      {
         gettingBoard.add(new Location(i, j), new BlackPiece());
       }
     }
-    for(int i=5; i <= 7; i = i +2)
+    for(int i=1; i <= 2; i = i +2)//for loop to add a row of black pieces
     {
       for(int j=1; j <= 7; j = j +2)
       {
         gettingBoard.add(new Location(i, j), new BlackPiece());
       }
     }
-    for(int i=3; i <= 4; i = i +2)
+    for(int i=4; i <= 7; i = i +2)//for loop to add two rows of white pieces
+    {
+      for(int j=0; j <= 7; j = j +2)
+      {
+        gettingBoard.add(new Location(i, j), new WhitePiece());
+      }
+    }
+    for(int i=5; i <= 7; i = i +2)//for loop to add a row of white pieces
+    {
+      for(int j=1; j <= 7; j = j +2)
+      {
+        gettingBoard.add(new Location(i, j), new WhitePiece());
+      }
+    }
+    for(int i=3; i <= 4; i = i +2)//for loop to add blank black squares to the grid as well 
     {
       for(int j=1; j <= 7; j = j +2)
       {
         gettingBoard.add(new Location(i, j), new BlackSquare());
       }
     }
-    for(int i=4; i <= 5; i = i +2)
+    for(int i=4; i <= 5; i = i +2)//another loop adding black squares 
     {
       for(int j=0; j <= 7; j = j +2)
       {
         gettingBoard.add(new Location(i, j), new BlackSquare());
       }
     }
-    gettingBoard.show();
+    gettingBoard.show();//display the world
   }
 }
--- GridWorldCode copy/Driver/framework/info/gridworld/actor/TestWorld.java
+++ GridWorldCode copy/Driver/framework/info/gridworld/actor/TestWorld.java
@@ -3,7 +3,8 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.Color;
+import javax.swing.JOptionPane
+  import java.awt.Color;
 import info.gridworld.world.World;
 import info.gridworld.grid.BoundedGrid;
 import info.gridworld.actor.ActorWorld;
@@ -16,17 +17,121 @@ import info.gridworld.grid.Location;
 
 public class TestWorld extends ActorWorld
 {
+  private boolean alreadyClicked = false;//making a boolean to determine if the first click was made
+  private Location loc = (0, 0);//creating a new location
+  private boolean turn = true;//making a boolean to keep track of who's turn it is
+  private boolean pieceIsKing = false;//keeps track if the piece is a king or not
+  
   public TestWorld()
   {
     super(new BoundedGrid(8, 8));
   }
   /* gets the location of the click (input from user) 
    * and offers a choice between left and right to move
-   * 
+   * */
   private boolean locationClicked()
   {
-    Location loc = display.getCurrentLocation();
-    System.out.println("Location clicked: " + loc);
-    return true;
-  }*/
-}
+    loc = display.getCurrentLocation();//get the location at the space clicked
+    setMessage("");
+    
+    if(!turn)//AI's turn
+    {
+      setMessage("Computer's turn");
+      return true;
+    }
+    
+    if(turn && !(loc.getActor() instanceof WhitePiece))//players can only move white pieces, prevents altering everything else
+    {
+      setMessage("Invalid click");
+      return true;
+    }
+    
+    if(turn && (loc.getActor() instanceof WhitePiece))//when it's the players turn and they click on a white piece
+    {
+      alreadyClicked = true;
+      setMessage("Where would you like to move this piece?");
+      return true;
+    }
+    
+    if(turn && (loc.getActor() instanceof WhiteKing))//when it's the players turn and they click on a white king
+    {
+      alreadyClicked = true;
+      pieceIsKing = true;
+      setMessage("Where would you like to move this piece?");
+      return true;
+    }
+    
+    if(alreadyClicked)//if the player has selected a valid piece
+    {
+      if(!pieceIsKing)//not a king
+      {
+        int moveDirection = JOptionPane.showConfirmDialog(null, "left", "right", JOptionPane.YES_NO_OPTION);//gives the option to move either left or right
+        if(moveDirection == JOptionPane.YES_OPTION)
+        {
+          boolean movable = validStep(int 1, loc);//call on private method validStep
+          if(movable)//if can move left, move left
+          {
+            loc.getActor().move();//calls on move to move piece
+          }
+          else if(!movable)//if can't move left
+          {
+            setMessage("You wasted a turn... sorry 'bout that");
+          }
+        }
+        else if(moveDirection == JOptionPane.NO_OPTION)
+        {
+          boolean movable = validStep(int 2, loc);//call on private method validStep
+          if(movable)//if can move right, move right
+          {
+            loc.getActor().move();//calls on move to move piece
+          }
+          else if(!movable)//if can't move right
+          {
+            setMessage("You wasted a turn... sorry 'bout that");
+          }
+        }
+      }
+      if(pieceIsKing)
+      {
+        int moveLeftRight = JOptionPane.showConfirmDialog(null, "left", "right", JOptionPane.YES_NO_OPTION);//gives the option to move either left or right
+        if(moveLeftRight == JOptionPane.YES_OPTION)
+        {
+          int moveUpDown = JOptionPane.showConfirmDialog(null, "up", "down", JOptionPane.YES_NO_OPTION);//gives the option to move either up or down
+          if(moveUpDown == JOptionPane.YES_OPTION)
+          {
+            boolean movable = validStep(int 3, loc);//call on private method validStep
+            if(movable)//if can move left and up, move left and up
+            {
+              loc.getActor().move();//calls on move to move piece
+            }
+            else if(!movable)//if can't move left and up
+            {
+              setMessage("You wasted a turn... sorry 'bout that");
+            }
+          }
+          if(moveUpDown == JOptionPane.NO_OPTION)
+          {
+            boolean movable = validStep(int 4, loc);//call on private method validStep
+            if(movable)//if can move left and down, move left and down
+            {
+              loc.getActor().move();//calls on move to move piece
+            }
+            else if(!movable)//if can't move left
+            {
+              setMessage("You wasted a turn... sorry 'bout that");
+            }
+          }
+        }
+        else if(moveDirection == JOptionPane.NO_OPTION)
+        {
+          boolean movable = validStep(int 2, loc);//call on private method validStep
+          if(movable)//if can move right, move right
+          {
+            loc.getActor().move();//calls on move to move piece
+          }
+          else if(!movable)//if can't move right
+          {
+            setMessage("You wasted a turn... sorry 'bout that");
+          }
+        }
+      }
